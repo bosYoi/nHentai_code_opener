a56be97e7e60b67b7864e914dae273f1e268911a
bosYoi <59323110+bosYoi@users.noreply.github.com>
2019-12-29 19:22:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ const goBtn = document.getElementById('openCodeBtn');
 goBtn.addEventListener('click', openLink);
 
 function openLink(){
-    window.open(`https://nhentai.net/g/${document.getElementById('codeInput').value}/`, '_blank');
+
+    if(document.getElementById('codeInput').value == ''){
+        return;
+    }
+
+    else if(isNaN(document.getElementById('codeInput').value)){
+        document.getElementById('codeInput').value = '';
+        return;
+    }
+
+    else{
+        window.open(`https://nhentai.net/g/${document.getElementById('codeInput').value}/`, '_blank');
+        document.getElementById('codeInput').value = '';
+    }
 }
