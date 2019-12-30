1ffd85bb9cd0fe1a888ba5c28f570e4a91ad2be8
InvictusTTV <44098372+InvictusTTV@users.noreply.github.com>
2019-12-30 22:56:10 +0530

    updated to check 404
    ...

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
-const goBtn = document.getElementById('openCodeBtn');
+var bt = document.getElementById("openCodeBtn");
+var code = document.getElementById("codeInput");
+// var link_rad = document.getElementById("link");
+// var code_rad = document.getElementById("code");
 
-goBtn.addEventListener('click', openLink);
+var cors = "https://cors-anywhere.herokuapp.com/";
 
-function openLink(){
+bt.onclick = function() {
+    
+    var ob = document.createElement("p");
+    ob.id = "message";
+    
+    var link = "https://nhentai.net/g/" + code.value;
 
-    if(document.getElementById('codeInput').value == ''){
-        return;
-    }
-
-    else if(isNaN(document.getElementById('codeInput').value)){
-        document.getElementById('codeInput').value = '';
-        return;
-    }
-
-    else{
-        window.open(`https://nhentai.net/g/${document.getElementById('codeInput').value}/`, '_blank');
-        document.getElementById('codeInput').value = '';
-    }
+    function checkCode(){
+        var send =  new XMLHttpRequest();
+        try{
+            if ("withCredentials" in send) {
+                send.open("get", cors + link);
+            }
+            send.send();
+            send.onreadystatechange = function(){
+                console.log(send.getAllResponseHeaders());
+                console.log(send.status);
+                if(send.status == 404)
+                {
+                    ob.innerHTML = "Kono hentai manga doesn't exist.";  
+                    document.getElementsByTagName("body")[0].insertBefore(ob, bt);
+                }
+                else if(send.status == 200)
+                {
+                    window.location.href = link;
+                }
+            }
+        }
+        catch(err)
+        {
+            ob.innerHTML = "Unresolved error occured."; 
+            document.getElementsByTagName("body")[0].insertBefore(ob, bt); 
+            console.log(err);
+        }
+    }  
+    checkCode();
 }
 
-document.addEventListener('keypress', function(e){
-    if(e.key == 'Enter'){
-        goBtn.click();
-    }
-})
+// link_rad.onclick = function(){
+//     code_rad.checked = false;
+// }
+    
+
+// code_rad.onclick = function(){
+//     link_rad.checked = false;
+// }
+    
+
